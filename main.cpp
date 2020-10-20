fec15ae9b80b563b77edf9b36f4259c37f8dad23
Ori4 <ori532@gmail.com>
2020-10-20 20:25:42 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+void funk1(int num) {
+    std::cout << "Hello, World! " << num << std::endl;
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, Fucking World!" << std::endl;
+    for (int i = 0; i < 200; ++i) {
+        funk1(i);
+    }
+    return 0;
+}
+
